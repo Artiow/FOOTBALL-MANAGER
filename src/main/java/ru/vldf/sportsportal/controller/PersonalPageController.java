07db514e17db0c18bce61432b1548a56274323ca
Artiow <namednev_a@mail.ru>
2018-01-19 21:12:57 +0300

    blank name fix

--- src/main/java/ru/vldf/sportsportal/controller/PersonalPageController.java
+++ src/main/java/ru/vldf/sportsportal/controller/PersonalPageController.java
@@ -1,24 +1,23 @@
 package ru.vldf.sportsportal.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.vldf.sportsportal.dto.UserDTO;
+import ru.vldf.sportsportal.controller.security.SecurityController;
 import ru.vldf.sportsportal.service.security.UserPrincipal;
 
 @Controller
-public class PersonalPageController {
+public class PersonalPageController extends SecurityController {
 
     @GetMapping(value = {"/personalpage"})
     public String personalPage(ModelMap map) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        UserPrincipal userPrincipal = (UserPrincipal) auth.getPrincipal();
-        UserDTO user = userPrincipal.getUser();
+        UserPrincipal principal = getPrincipal();
 
-        map.addAttribute("name", user.getName() + " " + user.getSurname());
+        String name;
+        if (principal != null) name = principal.getUser().getName() + " " + principal.getUser().getSurname();
+        else name = "ERROR";
+
+        map.addAttribute("name", name);
         return "personalpage";
     }
 }
